f2ef5ec6cd7c71844d6b2aac80ca05f2d100d5e2
woolennium <wooysong@yahoo.com>
2023-12-22 09:02:14 -0800

    Create learning.py

---
+++ learning.py
@@ -0,0 +1,27 @@
+def greet():
+    print("hi there")
+    print("welcome aboard")
+    print(2+2*10)
+    print("2+2*10")
+    # this is a comment
+greet()
+
+print('hello woo')
+
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
+
+print("number of minutes in a year")
+print(24*60*365)
+
+print(5, end="")
+print(" + ", end="")
+print(8, end="")
+print(" - ", end="")
+print(4, end="")
+print(" = ", end="")
+print(5 + 8 - 4)
+
+print(34005250 + 36725670 + 39446090 + 42166510 + 44886930)
